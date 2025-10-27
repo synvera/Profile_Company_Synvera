1edad43310de9c9260468356a9614db24c93d7ff
Mochammad Khahfi <synveragroup@gmail.com>
2025-10-27 20:33:27 +0700

    Update Skill

---
+++ script/index.js
@@ -0,0 +1,16 @@
+
+        const hamburger = document.getElementById('hamburger');
+        const navMenu = document.getElementById('navMenu');
+        const navLinks = document.querySelectorAll('.nav-link');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('active');
+            navMenu.classList.toggle('active');
+        });
+
+        navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                hamburger.classList.remove('active');
+                navMenu.classList.remove('active');
+            });
+        });
